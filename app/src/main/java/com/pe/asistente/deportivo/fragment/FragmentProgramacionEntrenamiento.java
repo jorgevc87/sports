8e7b5fd771c42ecffe837b15230a33c07bb0ef28
jorgevc87 <lacunacoil18@hotmail.com>
2015-06-16 21:38:44 -0500

    Cambios desde casa

--- app/src/main/java/com/pe/asistente/deportivo/fragment/FragmentProgramacionEntrenamiento.java
+++ app/src/main/java/com/pe/asistente/deportivo/fragment/FragmentProgramacionEntrenamiento.java
@@ -1,25 +1,17 @@
 package com.pe.asistente.deportivo.fragment;
 
-import android.graphics.RectF;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.CalendarView;
-import android.widget.Toast;
 
-import com.alamkanak.weekview.WeekView;
-import com.alamkanak.weekview.WeekViewEvent;
 import com.pe.asistente.deportivo.R;
 
-import java.util.List;
-
-public class FragmentProgramacionEntrenamiento extends Fragment implements WeekView.MonthChangeListener, WeekView.EventClickListener, WeekView.EventLongPressListener {
+public class FragmentProgramacionEntrenamiento extends Fragment {
 
     private View v;
-    private WeekView mWeekView;
 
     public static FragmentProgramacionEntrenamiento newInstance(Bundle bundle) {
         FragmentProgramacionEntrenamiento f = new FragmentProgramacionEntrenamiento();
@@ -42,34 +34,6 @@ public class FragmentProgramacionEntrenamiento extends Fragment implements WeekV
 
     private void incializarControles() {
 
-        //Get a reference for the week view in the layout.
-        mWeekView = (WeekView) v.findViewById(R.id.weekView);
-
-        // Set an action when any event is clicked.
-        mWeekView.setOnEventClickListener(this);
-
-        // The week view has infinite scrolling horizontally. We have to provide the events of a
-        // month every time the month changes on the week view.
-        mWeekView.setMonthChangeListener(this);
-
-        // Set long press listener for events.
-        mWeekView.setEventLongPressListener(this);
-
-
     }
 
-    @Override
-    public void onEventClick(WeekViewEvent weekViewEvent, RectF rectF) {
-
-    }
-
-    @Override
-    public void onEventLongPress(WeekViewEvent weekViewEvent, RectF rectF) {
-
-    }
-
-    @Override
-    public List<WeekViewEvent> onMonthChange(int i, int i2) {
-        return null;
-    }
 }
